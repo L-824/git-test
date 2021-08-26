27a912e3207578a36b8b6db7ae342d10d883971b
L-824 <915654291@qq.com>
2021-08-26 18:21:28 +0800

    修改FirstClass

--- git-test-project/src/com/itheima/git/FirstClass.java
+++ git-test-project/src/com/itheima/git/FirstClass.java
@@ -4,11 +4,13 @@ public class FirstClass {
 
     public static void main(String[] args) {
         System.out.println("hello git project");
+        // hello test
     }
 
     public void test() {
         int i = 0;
         i += 10;
+        System.out.println(i);
     }
 
 }
